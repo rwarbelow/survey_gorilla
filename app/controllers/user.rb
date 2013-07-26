dac35304e47869396ffda7041339199e5d550ef4
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-26 14:40:40 -0500

    Complete user and session routes

---
+++ app/controllers/user.rb
@@ -0,0 +1,26 @@
+## Sign up // create a new user
+get '/register' do
+end
+
+post '/register' do
+end
+
+## User Dashboard page
+get '/profile/:profile_id' do
+end
+
+## Edit user information
+get '/profile/:profile_id/edit' do
+end
+
+post '/profile/:profile_id/edit' do
+end
+
+
+## Delete your account
+get '/profile/:profile_id/delete' do
+end
+
+post '/profile/:profile_id/delete' do
+end
+
