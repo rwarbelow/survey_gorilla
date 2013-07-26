d4210b7cc579757a05d6cea9788fe70a4e89cf88
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-26 15:51:53 -0500

    created basic form for adding questions

--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,10 @@ class Response < ActiveRecord::Base
   has_many :votes, dependent: :destroy
 
   validates :text, presence: true
+
+  def self.create_from_params(string, question_id)
+    string.split(/,\w?/).each do |response|
+      Response.create(text: response, question_id: question_id)
+    end
+  end
 end
