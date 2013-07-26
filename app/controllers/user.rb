73ebe6cd18966c4d670892b49a4195eff7b9498a
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-26 16:35:31 -0500

    finish delete functionality and start edit functionality

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -12,20 +12,15 @@ post '/register' do
     @errors = @user.errors.full_messages
     erb :'user/register'
   end
-
-end
-
-## User Dashboard page
-get '/profile/:profile_id' do
-  if current_user.id == params[:profile_id]
-    erb :'user/dashboard.erb'
-  else
-    redirect '/' #change to 404 error, add 404 get to session
-  end
 end
 
 ## Edit user information
 get '/profile/:profile_id/edit' do
+  if current_user.id.to_i == params[:profile_id].to_i
+    erb :'user/edit_profile'
+  else
+    redirect '/' #change to 404 error, add 404 get to session
+  end
 end
 
 post '/profile/:profile_id/edit' do
@@ -34,8 +29,25 @@ end
 
 ## Delete your account
 get '/profile/:profile_id/delete' do
+    if current_user.id.to_i == params[:profile_id].to_i
+    erb :'user/confirm_delete'
+    else
+    redirect '/' #change to 404 error, add 404 get to session
+  end
 end
 
 post '/profile/:profile_id/delete' do
+  current_user.destroy
+  session[:id] = nil
+  redirect '/'
+end
+
+## User Dashboard page
+get '/profile/:profile_id' do
+  if current_user.id.to_i == params[:profile_id].to_i
+    erb :'user/dashboard'
+  else
+    redirect '/' #change to 404 error, add 404 get to session
+  end
 end
 
