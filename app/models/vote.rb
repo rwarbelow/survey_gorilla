c73b0f3f35116ab05dbf69cf56cf0ee7a609725a
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-28 14:20:02 -0500

    added button and two columns to index page

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :response
   delegate :question, to: :response
   delegate :survey, to: :response
+  validates_uniqueness_of :user_email, :scope => :response_id
 end
