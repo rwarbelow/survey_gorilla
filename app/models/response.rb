d3225d570ff3ac7d8e8e4e78a0aa09c12915a07e
Apprentice <apprentice@dbc-chicago-009.local>
2013-07-28 13:07:00 -0500

    added password confirmation, fixed divide by 0 error on percent

--- app/models/response.rb
+++ app/models/response.rb
@@ -8,6 +8,7 @@ class Response < ActiveRecord::Base
   def percent
     users_who_chose_response = self.votes.count
     users_who_participated = self.survey.participants.size
+    return 0 if users_who_participated == 0
     percent = (users_who_chose_response / users_who_participated.to_f * 100).round
   end
 
