6e62d744bcc8a8135100942156864cb223ee6cd5
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-28 17:53:49 -0500

    Comments out popular/last several view for styling purposes--no data causes errors

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  @new_surveys = Survey.order('created_at DESC').limit(20)
-  @popular_surveys = Survey.order('participant_count DESC').limit(20)
+  # @new_surveys = Survey.order('created_at DESC').limit(20)
+  # @popular_surveys = Survey.order('participant_count DESC').limit(20)
   erb :index
 end
